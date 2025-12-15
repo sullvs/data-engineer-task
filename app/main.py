105531aaa603b6f411438d90c83fc4ce24d833b3
sullvs <dlayelhd@gmail.com>
2025-12-16 01:36:06 +0300

    fill files with data

--- app/main.py
+++ app/main.py
@@ -2,20 +2,24 @@ import os
 import time
 import psycopg2
 
-conn = psycopg2.connect(
-    host=os.environ["DB_HOST"],
-    port=os.environ["DB_PORT"],
-    dbname=os.environ["DB_NAME"],
-    user=os.environ["DB_USER"],
-    password=os.environ["DB_PASSWORD"],
-)
+def wait_for_db():
+    while True:
+        try:
+            conn = psycopg2.connect(
+                host=os.environ["DB_HOST"],
+                port=os.environ["DB_PORT"],
+                dbname=os.environ["DB_NAME"],
+                user=os.environ["DB_USER"],
+                password=os.environ["DB_PASSWORD"],
+            )
+            conn.close()
+            print("Connected to PostgreSQL")
+            break
+        except Exception as e:
+            print("Waiting for PostgreSQL...", e)
+            time.sleep(2)
 
-print("Connected to PostgreSQL")
+wait_for_db()
 
 while True:
-    with conn.cursor() as cur:
-        cur.execute("SELECT NOW();")
-        result = cur.fetchone()
-        print("DB time:", result)
-
     time.sleep(5)
